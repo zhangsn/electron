385a05f99f50ea97e6fb2d09f44fe55857d8a842
zhangsn <>
2024-09-04 18:10:56 +0800

    multi render

---
+++ vite.renderer.main.config.ts
@@ -0,0 +1,27 @@
+import type { ConfigEnv, UserConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { pluginExposeRenderer } from './vite.base.config';
+import path from "path";
+
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<'renderer'>;
+  const { mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? '';
+
+  return {
+    root:path.join(__dirname, "src", "pages","main"),
+    mode,
+    base: './',
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    plugins: [pluginExposeRenderer(name),vue()],
+    resolve: {
+      preserveSymlinks: true,
+    },
+    clearScreen: false,
+  } as UserConfig;
+});
